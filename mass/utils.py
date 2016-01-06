13ff07bc492f6e92204d9f860b712f29d5d08274
badboy99tw <taiwan.badboy99@gmail.com>
2016-01-06 22:52:56 +0800

    Import swf config only if scheduler is 'swf'.

--- mass/utils.py
+++ mass/utils.py
@@ -10,7 +10,6 @@ import json
 # local modules
 from mass.exception import UnsupportedScheduler
 from mass.input_handler import InputHandler
-from mass.scheduler.swf import config
 
 
 def submit(job, protocol=None, priority=1, scheduler='swf'):
@@ -18,6 +17,7 @@ def submit(job, protocol=None, priority=1, scheduler='swf'):
     """
     if scheduler != 'swf':
         raise UnsupportedScheduler(scheduler)
+    from mass.scheduler.swf import config
     import boto3
     client = boto3.client('swf', region_name=config.REGION)
     handler = InputHandler(protocol)
